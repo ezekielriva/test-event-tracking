95be64acfb8f726f5ede91a9cb6537394d0dd9a5
Ezekiel <ezerivadeneiral@gmail.com>
2013-11-06 13:42:11 -0300

    Adds chart generation for a simple poll

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,17 +4,6 @@
 window.onload = function(){
     oo.setAPIKey("1c8a535f118b26a6974462c7bd84e71e3c6cbbe4");
 
-    oo.load(function(){
-        //UA-45490920-1
-        var metric = new oo.Metric("78741731", "30d");
-
-        metric.setMetric("ga:visits");
-
-        metric.draw('visits');
-
-
-    });
-
     var submit = document.getElementById('submit-poll');
     submit.onclick = function(e) {
       e.preventDefault();
@@ -31,6 +20,29 @@ window.onload = function(){
         console.error('Track event wasn\'t added');
       } else {
         console.log('Track event was added');
+        drawChart(form.id);
       }
+
+    };
+
+    var drawChart = function(form) {
+
+      oo.load(function(){
+          var metric = new oo.Metric("78741731", "30d");
+          metric.setMetric("ga:visits");
+          metric.draw('visits');
+
+          var bar = new oo.Bar("78741731", "30d");
+          bar.query.setFilter('ga:eventCategory=='+form);
+
+          bar.addMetric("ga:totalEvents", "Votes");
+
+          bar.setDimension("ga:eventCategory");
+          bar.setDimension("ga:eventAction");
+          bar.setDimension("ga:eventLabel");
+
+          bar.draw('chart-poll-results');
+
+      });
     };
 };
